90ff7f6db165d293ed91b864d87f4feb9706efd7
Peter LeFanu Lumsdaine <p.l.lumsdaine@gmail.com>
2021-07-26 18:04:45 +0200

    Starting with easy one: largest powers, the obvious lazy way

--- h4-largest-powers/src/Lib.hs
+++ h4-largest-powers/src/Lib.hs
@@ -15,8 +15,34 @@ instance Iterable Int where
     inc = succ
     dec = pred
 
+instance Iterable Integer where
+    zer = 0
+    inc = succ
+    dec = pred
+
 largestPowersInt :: Int -> [Int]
 largestPowersInt = largestPowers
 
-largestPowers :: Iterable a => Int -> [a]
-largestPowers = undefined
+largestPowers0 :: Iterable a => Int -> [a]
+largestPowers0 n = map inc lpn
+  where
+    lpn = replicate (n-1) zer ++ ntersperse (map inc lpn)
+    ntersperse (a:as) = (a : replicate (n-1) zer) ++ (ntersperse as)
+
+largestPowers1 :: Iterable a => Int -> [a]
+largestPowers1 n = map inc lpn
+  where
+    lpn = replicate (n-1) zer ++ foldr (\a l -> a : replicate (n-1) zer ++ l) [] (map inc lpn)
+
+largestPowers2 :: Iterable a => Int -> [a]
+largestPowers2 n = lpn
+  where
+    lpn = replicate (n-1) (inc zer) ++ foldr (\a l -> a : replicate (n-1) (inc zer) ++ l) [] (map inc lpn)
+
+largestPowers3 :: Iterable a => Int -> [a]
+largestPowers3 n = lpn
+  where
+    lpn = seg ++ foldr (\a l -> a : seg ++ l) [] (map inc lpn)
+    seg = replicate (n-1) (inc zer)
+    
+largestPowers = largestPowers3
